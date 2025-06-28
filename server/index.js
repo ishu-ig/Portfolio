2be86ce27c9b8d02dccd3a943b84d22742b91169
ishu-ig <ishaanguptacse@gmail.com>
2025-06-28 14:44:58 +0530

    build added in admin

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require("./db_connect");                // ✅ Connect to DB
 const app = express();
 const Router = require("./routes/index");
 
-const whitelist = ['http://localhost:4000', 'http://localhost:8000', 'http://localhost:3000'];
+const whitelist = ['http://localhost:4000', 'http://localhost:8000', 'http://localhost:3000','https://portfolio-g2rt.onrender.com'];
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || whitelist.includes(origin)) {
